39bcbe57b7da5d23e60807b1ac8501a7bbcc4809
Jonas <Jonas@DESKTOP-KVPLKCJ>
2018-11-04 20:41:03 +0100

    fixed ItemStackDeserializer

--- src/blockgame/model/ItemStackDeserializer.java
+++ src/blockgame/model/ItemStackDeserializer.java
@@ -5,10 +5,31 @@
  */
 package blockgame.model;
 
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+import java.lang.reflect.Type;
+
 /**
  *
  * @author Jonas
  */
-public class ItemStackDeserializer {
+public class ItemStackDeserializer implements JsonDeserializer<ItemStack>{
+
+    @Override
+    public ItemStack deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        JsonObject jObject = json.getAsJsonObject();
+        
+        // bron deserialize nested objects: http://www.javacreed.com/gson-deserialiser-example/
+        Item item = context.deserialize(jObject.get("item"), Item.class);
+        int amount = jObject.get("amount").getAsInt();
+        
+        return new ItemStack(item, amount);
+    }
+    
+    
+    
     
 }
