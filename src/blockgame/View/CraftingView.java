72a07126a0d3c003998e795235f8cde578440543
Souhaib El Habti <Souhaib El Habti@DESKTOP-V4BQ962.lan>
2018-11-28 21:41:31 +0100

    crafting Uiterlijk

--- src/blockgame/View/CraftingView.java
+++ src/blockgame/View/CraftingView.java
@@ -6,11 +6,17 @@
 package blockgame.View;
 
 import blockgame.model.Crafting;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
 import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 
 /**
  *
@@ -30,7 +36,19 @@ public class CraftingView extends Region{
         r.setX(646);
         r.setY(35);
         
-        getChildren().addAll(r);
+        Label l = new Label("Crafting Menu");
+        Font f = new Font(35);
+        l.setFont(f);
+        l.setTextFill(Color.rgb(255,255,255));
+        l.setTranslateX(790);
+        l.setTranslateY(40);
+        
+        /*
+            Rectangle rc = new Rectangle(300, 40, Color.rgb(134,136,138));
+            
+        */
+        
+        getChildren().addAll(r, l);
     }
     
 }
