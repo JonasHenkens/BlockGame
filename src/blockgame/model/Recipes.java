89135ddab86b24425bad85ef945bd8dc94d9f92d
Souhaib El Habti <Souhaib El Habti@DESKTOP-V4BQ962.lan>
2018-12-06 10:35:07 +0100

    recipes

--- src/blockgame/model/Recipes.java
+++ src/blockgame/model/Recipes.java
@@ -5,10 +5,58 @@
  */
 package blockgame.model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Souhaib El Habti
  */
 public class Recipes {
+    private ArrayList<Item> product;
+    private ArrayList<Item> ingredients;
+
+    public Recipes() {
+        product = new ArrayList<>();
+        ingredients = new ArrayList<>();
+    }
+
+    /**
+     * @param item the product you want to add
+     */
+    public void addProduct(Item item) {
+        if(!product.contains(item)){
+            product.add(item);
+        }
+    }
+
+    /**
+     * @param item the ingredients to set
+     */
+    public void addIngredients(Item item) {
+        if(!ingredients.contains(item)){
+            ingredients.add(item);
+        }
+    }
+
+    /**
+     * @return the product
+     */
+    public Item getProduct(int place) {
+        return product.get(place);
+    }
+
+    /**
+     * @return the ingredients
+     */
+    public Item getIngredients(int place) {
+        return product.get(place);
+    }
+    
+    
+    
+    
+    
+    
+    
     
 }
