8f64cfc6d75f906fba68081c5603e5f376fe5d08
Souhaib El Habti <Souhaib El Habti@DESKTOP-V4BQ962.lan>
2018-12-13 21:14:34 +0100

    Inventorytest

--- test/blockgame/model/InventoryTest.java
+++ test/blockgame/model/InventoryTest.java
@@ -44,9 +44,9 @@ public class InventoryTest {
      */
     @Test
     public void testAddItemInInventory() {
-        // Random id: from 0 to 3
+        // Random id: from 0 to 4
         // Random amount: 1 or 2
-        int id = (int) (Math.random()*4);
+        int id = (int) (Math.random()*5);
         int amount = (int) (Math.random()*2)+1;
         
         // Inventory is empty, so everything can be placed in it
@@ -54,9 +54,9 @@ public class InventoryTest {
         boolean result = inv.addItemInInventory(id, ItemType.tool , amount);
         assertEquals("Didn't add the tool(s)",expResult, result);
         
-        // Random id: from 0 to 9
+        // Random id: from 0 to 3
         // Random amount: from 1 to 40
-        id = (int) (Math.random()*10);
+        id = (int) (Math.random()*4);
         amount = (int) (Math.random()*40)+1;
         
         // Inventory has 1 or 2 places left, so everything can be placed in it 
@@ -64,37 +64,39 @@ public class InventoryTest {
         boolean result2 = inv.addItemInInventory(id, ItemType.material , amount);
         assertEquals("Didn't add the materials",expResult2, result2);
         
-        // Random id: from 0 to 2
+        // Random id: from 0 to 9
         // Random amount: from 1 to 40
-        id = (int) (Math.random()*3);
+        id = (int) (Math.random()*10);
         amount = (int) (Math.random()*40)+1;
         
         // If Inventory has 1 place left, then the expectation is true
-        //Else the expectation becomes false
-        boolean expResult3;
+        // Else the expectation becomes false
+        boolean expResult3 = true;
         if(inv.getAmountInItemStack(2)>0){
             expResult3 = false;
         }
-        else{
-            expResult3 = true;
-        }
         boolean result3 = inv.addItemInInventory(id, ItemType.block, amount);
-        assertEquals("Didn't work",expResult3, result3);      
+        assertEquals("Didn't work",expResult3, result3); 
     }
 
-    /**
         @Test
         public void testRemoveItemInInventory() {
-            System.out.println("removeItemInInventory");
-            int id = 0;
-            ItemType type = null;
-            int amount = 0;
-            Inventory instance = null;
-            boolean expResult = false;
-            boolean result = instance.removeItemInInventory(id, type, amount);
-            assertEquals(expResult, result);
-            // TODO review the generated test code and remove the default call to fail.
-            fail("The test case is a prototype.");
+        // Random id: from 0 to 4 & amount: 1 tool
+        // Add tool in inventory
+        int id = (int) (Math.random()*5);
+        int amount = 1;
+        inv.addItemInInventory(id, ItemType.tool , amount);
+   
+        // Random id: from 0 to 3 & Random amount: from 1 to 40
+        // Add materials in inventory
+        id = (int) (Math.random()*4);
+        amount = (int) (Math.random()*40)+1;
+        inv.addItemInInventory(id, ItemType.material , amount);
+        
+        // Random id: from 0 to 9 & Random amount: from 1 to 40
+        // Add blocks in inventory
+        id = (int) (Math.random()*10);
+        amount = (int) (Math.random()*40)+1;
+        inv.addItemInInventory(id, ItemType.block, amount);
         }
-    */
 }
